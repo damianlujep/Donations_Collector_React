fab9b995bf20286eb32d2a4bdc65f78473d5f190
damianlujep <Comecalzonn1>
2021-09-18 02:39:24 +0200

    Three columns statistics - Complete

---
+++ src/components/home/HomeThreeColumns.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const HomeThreeColumns = () => {
+    return (
+        <div className="home-statistics">
+            <article>
+                <span>10</span>
+                <p>Oddanych worków</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisc
+                    Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
+                </p>
+            </article>
+            <article>
+                <span>5</span>
+                <p>Wspartych organizacji</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisc
+                    Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
+                </p>
+            </article>
+            <article>
+                <span>7</span>
+                <p>Zorganizowany zbiórek</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisc
+                    Pellentesque vel enim a elit viverra elementuma.
+                    Aliquam erat volutpat.
+                </p>
+            </article>
+        </div>
+    );
+};
+
+export default HomeThreeColumns;
